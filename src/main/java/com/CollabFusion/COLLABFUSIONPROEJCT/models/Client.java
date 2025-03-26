cb80a55a8dea900469a4915cbd6f84a96899ca09
Rachit <rachitdua997@gmail.com>
2025-03-27 00:50:37 +0530

    Update Client.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/Client.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/Client.java
@@ -1,59 +1,59 @@
-package com.CollabFusion.COLLABFUSIONPROEJCT.models;
+// package com.CollabFusion.COLLABFUSIONPROEJCT.models;
 
-public class Client {
+// public class Client {
 
-    private int id;
-    private String title;
-    private String contributorName;
-    private String email;
-    private String publisherName;
-    private String createdAt;
+//     private int id;
+//     private String title;
+//     private String contributorName;
+//     private String email;
+//     private String publisherName;
+//     private String createdAt;
 
-    public int getId() {
-        return id;
-    }
+//     public int getId() {
+//         return id;
+//     }
 
-    public void setId(int id) {
-        this.id = id;
-    } 
+//     public void setId(int id) {
+//         this.id = id;
+//     } 
 
-    public String getTitle() {
-        return title;
-    }
+//     public String getTitle() {
+//         return title;
+//     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+//     public void setTitle(String title) {
+//         this.title = title;
+//     }
 
-    public String getContributorName() {
-        return contributorName;
-    }
+//     public String getContributorName() {
+//         return contributorName;
+//     }
 
-    public void setContributorName(String contributorName) {
-        this.contributorName = contributorName;
-    }
+//     public void setContributorName(String contributorName) {
+//         this.contributorName = contributorName;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getPublisherName() {
-        return publisherName;
-    }
+//     public String getPublisherName() {
+//         return publisherName;
+//     }
 
-    public void setPublisherName(String publisherName) {
-        this.publisherName = publisherName;
-    }
+//     public void setPublisherName(String publisherName) {
+//         this.publisherName = publisherName;
+//     }
 
-    public String getCreatedAt() {
-        return createdAt;
-    }
+//     public String getCreatedAt() {
+//         return createdAt;
+//     }
 
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-}
+//     public void setCreatedAt(String createdAt) {
+//         this.createdAt = createdAt;
+//     }
+// }
