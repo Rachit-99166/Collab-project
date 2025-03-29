2d48d995029ba1db86e58a35fb90d2bdf087de78
Rachit <rachitdua997@gmail.com>
2025-03-29 13:00:27 +0530

    Update AccountController.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/AccountController.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.CollabFusion.COLLABFUSIONPROEJCT.models.RegisterDto;
-import com.CollabFusion.COLLABFUSIONPROEJCT.models.customerUser;
+import com.CollabFusion.COLLABFUSIONPROEJCT.models.customerUser; 
 import com.CollabFusion.COLLABFUSIONPROEJCT.repositories.customerUserRepository;
 
 import jakarta.validation.Valid;
