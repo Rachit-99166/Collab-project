7b582a8d3d5fbdb763329d740b89a4bb66a17c89
Rachit <rachitdua997@gmail.com>
2025-04-03 11:33:22 +0530

    Update ClientsRepository.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/repositories/ClientsRepository.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/repositories/ClientsRepository.java
@@ -35,45 +35,5 @@ public class ClientsRepository {
         return clients;
     }
 
-    // public Client getClient(int id) {
-    //     String sql = "SELECT * FROM clients WHERE id = ?";
-    //     SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, id);
-    //     if (rows.next()) {
-    //         Client client = new Client();
-    //         client.setId(rows.getInt("id"));
-    //         client.setTitle(rows.getString("title"));
-    //         client.setContributorName(rows.getString("contributor_name"));
-    //         client.setEmail(rows.getString("email"));
-    //         client.setPublisherName(rows.getString("publisher_name"));
-    //         client.setCreatedAt(rows.getString("created_at"));
-    //         return client;
-    //     }
-    //     return null;
-    // }
-    // public Client createClient(Client client) {
-    //     String sql = "INSERT INTO clients (title, contributor_name, email," + " publisher_name, created_at) VALUES (?, ?, ?, ?, ?)";
-    //     int count = jdbcTemplate.update(
-    //             sql,
-    //             client.getTitle(),
-    //             client.getContributorName(),
-    //             client.getEmail(),
-    //             client.getPublisherName(),
-    //             client.getCreatedAt());
-    //     if (count > 0) {
-    //         int id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
-    //         return getClient(id);
-    //     }
-    //     return null;
-    // }
-    // public Client updateClient(Client client) {
-    //     String sql = "UPDATE clients SET title = ?, contributor_name = ?, email = ?," + "publisher_name = ?, created_at = ? WHERE id = ?";
-    //     jdbcTemplate.update(sql,
-    //             client.getTitle(),
-    //             client.getContributorName(),
-    //             client.getEmail(),
-    //             client.getPublisherName(),
-    //             client.getCreatedAt(),
-    //             client.getId());
-    //     return getClient(client.getId());
-    // }
+    
 }
