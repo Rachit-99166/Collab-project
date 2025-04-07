c071fb72fcd1114761046b0e249f16202cee7e40
Rachit <rachitdua997@gmail.com>
2025-04-07 09:54:21 +0530

    Update loginController.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/loginController.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/controllers/loginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class loginController {
-
+ 
     @GetMapping({"", "/home"})
     public String home() {
         return "home";
