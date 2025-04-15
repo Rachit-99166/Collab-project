0c2aa8c78d7a8b79c64aecf2333c960fa9961b00
Rachit <rachitdua997@gmail.com>
2025-04-16 00:33:56 +0530

    Update customerUser.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/customerUser.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/models/customerUser.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "users")
 public class customerUser { 
-
+ 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
