88614433bfc44ef12fe372b9a477a98d53a21353
Rachit <rachitdua997@gmail.com>
2025-05-07 00:00:50 +0530

    Update ClientsRepository.java

--- src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/repositories/ClientsRepository.java
+++ src/main/java/com/CollabFusion/COLLABFUSIONPROEJCT/repositories/ClientsRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate; 
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
